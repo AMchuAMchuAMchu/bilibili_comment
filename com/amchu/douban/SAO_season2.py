85bd72cfcf4e3d435543f9a4fd7d8c5376ce705d
AMchuAMchuAMchu <2466130840@qq.com>
2022-11-06 10:18:30 +0800

    AM 01st commit

---
+++ com/amchu/douban/SAO_season2.py
@@ -0,0 +1,24 @@
+# Description ==> TODO
+# BelongsProject ==> bilibili_comment
+# BelongsPackage ==> 
+# Version ==> 1.0
+# CreateTime ==> 2022-11-06 10:06:09
+# Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+
+import requests
+import json
+from bs4 import BeautifulSoup
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"}  # 伪装成浏览器，绕过反爬
+
+
+res01 = requests.request(method='get',url='https://movie.douban.com/subject/25804168/comments?percent_type=&start=20&limit=20&status=P&sort=new_score&comments_only=1',headers=headers)
+
+print(res01.ok)
+
+res01.encoding = 'utf-8'
+
+context = json.loads(res01.text)
+
+print(context)
---
+++ com/amchu/douban/__init__.py
@@ -0,0 +1,6 @@
+# Description ==> TODO
+# BelongsProject ==> bilibili_comment
+# BelongsPackage ==> 
+# Version ==> 1.0
+# CreateTime ==> 2022-11-06 10:05:52
+# Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
